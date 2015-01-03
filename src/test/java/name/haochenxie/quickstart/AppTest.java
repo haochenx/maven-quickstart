ef9f35f44940ce88fc15d90237a00bea2ca43bb9
Haochen Xie <haochen@xie.name>
2015-01-03 21:16:47 +0900

    Add Maven skeleton project
    Signed-off-by: Haochen Xie <haochen@xie.name>

---
+++ src/test/java/name/haochenxie/quickstart/AppTest.java
@@ -0,0 +1,18 @@
+package name.haochenxie.quickstart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
